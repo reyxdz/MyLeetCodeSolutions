213c9870e415d874c78b2a1f4ab88d6454ef7462
reyxdz <reyfdenzo@outlook.com>
2025-10-16 07:51:41 -0400

    1. Two Sum solution using C#. Difficulty: Easy

---
+++ C#/Easy/1-Two_Sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int length = nums.Length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[] {};
+    }
+}
