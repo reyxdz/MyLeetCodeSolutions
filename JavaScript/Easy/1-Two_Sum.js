747f6e82ac929072c176fa3f864c7abe280df8e4
reyxdz <reyfdenzo@outlook.com>
2025-10-16 07:44:12 -0400

    1. Two Sum solution using JavaScript. Difficulty: Easy

---
+++ JavaScript/Easy/1-Two_Sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+    const length = nums.length;
+
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+
+    return [];
+};
